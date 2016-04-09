dce5ef46d88d2112256f15f40eb6d47f4035645d
Sayan Samanta <sayansamanta94@gmail.com>
2016-04-09 10:36:23 +0530

    Java script added

---
+++ scripts/validate.js
@@ -0,0 +1,28 @@
+function  check_pass() {
+    var x = document.forms["reg_form"]["n_passwd"].value;
+    var y = document.forms["reg_form"]["re_passwd"].value;
+    if (x === y) {
+        return true;
+    } else {
+        alert("Not Matched");
+        return false;
+    }
+}
+function check_balance() {
+    var x = document.forms["reg_form"]["ac_info"].value;
+    if (x < 0) {
+        alert("Enter Ammount Grater than Zero!");
+        return false;
+    } else {
+        return true;
+    }
+}
+function validate() {
+    if (check_pass() && check_balance()) {
+        return true;
+        alert("some error");
+    } else {
+        return false;
+    }
+}
+
